7aeb0fc2ff33f9ff3f18e6c8244b9f257da4cfa9
antoniotassone94 <antonio.tassone@studio.unibo.it>
2022-11-25 09:53:17 +0100

    aggiornamento componente giornatacalendario (template html, stile css e codice typescript)

--- src/app/componenti/giornatacalendario/giornatacalendario.component.ts
+++ src/app/componenti/giornatacalendario/giornatacalendario.component.ts
@@ -14,13 +14,14 @@ export class GiornatacalendarioComponent implements OnInit {
   constructor(){}
 
   ngOnInit():void{
-    for (let i = 0; i < this.matches.length; i++) {
+    for (let i:number = 0; i < this.matches.length; i++) {
       let match = this.matches[i];
-      for (let j = 0; j < match.teams.length; j++) {
+      for (let j:number = 0; j < match.teams.length; j++) {
         let team = match.teams[j];
         team.color = "rgb(" + team.color + ")";
         team.logo = "assets/images/" + team.logo;
-        team.themeImage = "url(\"assets/images/" + team.themeImage + ".png\")";
+        team.themeImage = "assets/images/" + team.themeImage + ".png";
+        team.conference = "(" + team.conference + ")";
       }
     }
   }
