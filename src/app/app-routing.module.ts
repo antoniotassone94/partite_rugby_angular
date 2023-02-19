56e032678cce37f6c56e7f29de1100f08d693f93
antoniotassone94 <antonio.tassone@studio.unibo.it>
2023-02-19 16:30:34 +0100

    risolto bug di visualizzazione dell'homepage (adesso il calendario delle partite viene visualizzato una sola volta, prima invece era stampato due volte)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import {NgModule} from "@angular/core";
 import {RouterModule,Routes} from "@angular/router";
-import {HomepageComponent} from "./componenti/homepage/homepage.component";
+import {CalendariopartiteComponent} from "./componenti/calendariopartite/calendariopartite.component";
 import {Errore404Component} from "./componenti/errore404/errore404.component";
 
 const routes: Routes = [
   {path:"",pathMatch:"full",redirectTo:"/homepage"},
-  {path:"homepage",component:HomepageComponent},
+  {path:"homepage",component:CalendariopartiteComponent},
   {path:"404",component:Errore404Component},
   {path:"**",redirectTo:"/404"}
 ];
